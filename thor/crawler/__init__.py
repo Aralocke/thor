3f4de7f08abd1fb14326ccdf83e7ca548f2bb67f
Daniel Weiner <danny@phantomnet.net>
2013-10-30 17:53:36 -0700

    Reworked the service system and split service and crawler into seperate packages

---
+++ thor/crawler/__init__.py
@@ -0,0 +1,2 @@
+__author__="dweiner"
+__date__ ="$Oct 30, 2013 3:55:19 PM$"
---
+++ thor/crawler/urlparse.py
@@ -0,0 +1,9 @@
+from urlparse import urlparse
+
+class Urlparse(object):
+    
+    def __init__(self, url):
+        pass
+    
+    def __str__(self):
+        return '[URL ]'
