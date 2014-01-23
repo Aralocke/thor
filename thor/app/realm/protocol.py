4f55b2e7645fd8081723be0fdcb90f8fd31de899
Daniel Weiner <danny@phantomnet.net>
2014-01-22 17:01:26 -0800

    Created encode/decode functions for JSON internal communication

--- thor/app/realm/protocol.py
+++ thor/app/realm/protocol.py
@@ -1,3 +1,5 @@
+import simplejson as json
+
 from twisted.protocols import basic
 from twisted.python import log
 from thor.common.core import object
@@ -35,3 +37,22 @@ class ServerClientProtocol(ClientProtocol):
 		# Also we should notify someone .. or something that a connection 
 		# has been quietly lost. FOr now we will just print a message out
 		ClientProtocol.connectionLost(self, reason)
+
+__separators = (',', ':')
+__sort_keys = False
+
+def decode(obj):
+	return json.loads(str(obj))
+
+def encode(obj, **kwargs):
+	# First step is to process for predefined separators of the string
+	# by default we use traditional ones that are globally set
+	if hasattr(kwargs, 'separators'):
+		separators = getattr(kwargs, 'separators', __separators)
+	# We also have the option to sort the keys within a JSON string. For 
+	# our purposes this is not required because we are not printing these
+	# strings for readability
+	if hasattr(kwargs, 'sort_keys'):
+		sort_keys = getattr(kwargs, 'sort_keys', __separators)
+
+	return json.dumps(obj, separators=separators, sort_keys=sort_keys)
